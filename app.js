ee2ad5b678b3573bef0e5ae3672403c0e1ad1af2
Chris Mayer <mayerxc@gmail.com>
2017-02-19 21:57:04 -0500

    added node_modules to .gitignore

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var todoController = require('./controllers/todoController.js');
+
+
+//set up template engine
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static(__dirname + '/public')); // to get the css file, you would now only need ./assets/styles.css, and not the public also
+
+//fire controllers
+todoController(app);
+
+//listen to port
+app.listen(3000);
+console.log('You are listening to port 3000');
